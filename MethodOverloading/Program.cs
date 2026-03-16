f93b91d3784002ea5682f4a2a7f86b606702754e
Ruben Gonzalez <rubengz23@yahoo.com>
2026-03-15 21:00:36 -0700

    Assignment Save

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,42 @@
 {
     public class Program
     {
+        public static int Add(int numOne, int numTwo)
+        {
+            return (numOne + numTwo);
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return (numOne + numTwo);
+        }
+
+        public static string Add(int numOne, int numTwo, bool isMoney)
+        {
+            var sum = numOne + numTwo;
+            if (isMoney == true && sum > 0)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return ("Zero Dollars");
+                
+            }
+        }
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //First Add Method
+            Console.WriteLine(Add(5, 5));
+            
+            //Second Add Method
+            Console.WriteLine(Add(5.5m, 5.5m));
+
+            //Third Add Method
+            Console.WriteLine(Add(0, -2, true));
+
         }
     }
 }
